a33a89cd4179a6e2799469fda81aa93fe0d5741f
Bruno Aguiar <bfmaguiar@gmail.com>
2026-05-07 15:33:35 +0100

    Comentarios ao codigo

--- Mod4_4_Commits.py
+++ Mod4_4_Commits.py
@@ -1,7 +1,14 @@
 # Programa que pede dois números inteiros e verifica se são iguais ou diferentes
 
+# Pedir o primeiro número ao utilizador
+
 numero_1 = int(input("Introduz um número: "))
+
+# Pedir o segundo número ao utilizador
+
 numero_2 = int(input("Introduz outro número: "))
+
+# Verifica se os números são iguais ou diferentes
 
 if numero_1 == numero_2:
     print("Os números introduzidos são iguais.")
